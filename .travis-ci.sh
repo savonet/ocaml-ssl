b3a4b0a666bc591a00f931f2a2f8500c51ca7eb4
Samuel Mimram <smimram@gmail.com>
2015-08-25 13:48:12 +0200

    Correct bytes module for travis.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 # Hacking the build into Travis-CI "C" environment
 # See http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
 
-OPAM_PACKAGES='ocamlfind bytes'
+OPAM_PACKAGES='ocamlfind base-bytes'
 
 export OPAMYES=1
 opam init
