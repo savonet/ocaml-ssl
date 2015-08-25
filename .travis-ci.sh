087b2390f9135a51f8dfbc545cb83d3f70755fa6
Samuel Mimram <smimram@gmail.com>
2015-08-25 12:55:52 +0200

    Use opam for travis.
    The bytes package is not present in Ubuntu...

---
+++ .travis-ci.sh
@@ -0,0 +1,14 @@
+# Hacking the build into Travis-CI "C" environment
+# See http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
+
+OPAM_PACKAGES='ocamlfind bytes'
+
+opam init
+if [ -n "${OPAM_SWITCH}" ]; then
+    opam switch ${OPAM_SWITCH}
+fi
+eval `opam config env`
+opam install -q -y ${OPAM_PACKAGES}
+
+# compile & run tests
+./bootstrap && ./configure && make
