3813187e17c097e5f78b53cfbe8b61f81e38ee3e
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-08-12 11:34:05 -0700

    remove h2spec

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -12,13 +12,10 @@ let
   pkgs = import "${src}" {
     extraOverlays = [
       (self: super: {
-        h2spec = super.callPackage ../h2spec.nix { };
         ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}";
       })
     ];
   };
 
-
-  inherit (pkgs) callPackage lib stdenv fetchTarball ocamlPackages h2spec;
 in
-callPackage ./.. { doCheck = true; }
+pkgs.callPackage ./.. { doCheck = true; }
