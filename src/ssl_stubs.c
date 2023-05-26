2f0b929940bc1c12b07eee986c637a2c1bde276b
Ezequiel Lewin <ezequiel.lewin@riskoptics.com>
2023-05-26 20:57:49 -0300

    Style fix
    Co-authored-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- src/ssl_stubs.c
+++ src/ssl_stubs.c
@@ -291,8 +291,8 @@ CAMLprim value ocaml_ssl_get_error_struct(value unit)
 CAMLprim value ocaml_ssl_peek_error_last_struct(value unit)
 {
   CAMLparam1(unit);
-  unsigned long code = ERR_peek_last_error();
   CAMLlocal3(result, libstring, reasonstring);
+  unsigned long code = ERR_peek_last_error();
   result = caml_alloc_tuple(3);
   const char *lib = ERR_lib_error_string(code);
   const char *reason = ERR_reason_error_string(code);
