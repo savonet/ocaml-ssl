c6d8bd758f776082f894dd7f4891a78bf4c8e5ca
Christophe Raffalli <christophe@raffalli.eu>
2023-06-14 19:20:43 -1000

    cleaned the runtime_lock tests

--- tests/ssl_rlock_io.ml
+++ tests/ssl_rlock_io.ml
@@ -1,47 +1,53 @@
 open Alcotest
 
+module Util = Util_rlock
+
 module Ssl = struct
   include Ssl
   include Ssl.Runtime_lock
-end
 
-module Util = Util_rlock
+  let open_connection_with_context context sockaddr =
+    let domain = Unix.domain_of_sockaddr sockaddr in
+    let sock = Unix.socket domain Unix.SOCK_STREAM 0 in
+    try
+      Unix.connect sock sockaddr;
+      Unix.set_nonblock sock;
+      let ssl = embed_socket sock context in
+      Util.loop (fun () -> connect ssl);
+      ssl
+    with
+    | exn ->
+      Unix.close sock;
+      raise exn
+
+
+end
 
 let test_verify () =
   let addr = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 2342) in
   Util.server_thread addr None |> ignore;
-
   let context = Ssl.create_context TLSv1_3 Client_context in
-  let ssl = Ssl.open_connection_with_context context addr in
+  let ssl = Util.loop (fun _ -> Ssl.open_connection_with_context context addr) in
   let verify_result =
     try
-      Ssl.verify ssl;
-      ""
+      Util.loop (fun () -> Ssl.verify ssl; "")
     with
     | e -> Printexc.to_string e
   in
-  let rec fn () =
-    try
-      Ssl.shutdown_connection ssl;
-    with
-      Ssl.(Connection_error(Error_want_write|Error_want_read|
-                         Error_want_accept|Error_want_connect|Error_zero_return)) ->
-      fn ()
-  in
-  fn ();
+  Util.loop (fun () -> Ssl.shutdown_connection ssl);
   check
     bool
     "no verify errors"
     true
     (Str.search_forward
-       (Str.regexp_string "error:00000000:lib(0)")
+       (Str.regexp_string "error:00:000000:lib(0)::reason(0)")
        verify_result
        0
     > 0)
 
 let test_set_host () =
   let addr = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 2343) in
-  let pid = Util.server_thread addr None in
+  let _ = Util.server_thread addr None in
 
   let context = Ssl.create_context TLSv1_3 Client_context in
   let domain = Unix.domain_of_sockaddr addr in
@@ -50,70 +56,37 @@ let test_set_host () =
   Ssl.set_host ssl "localhost";
   Unix.connect sock addr;
   Unix.set_nonblock sock;
-  let rec fn () =
-    try
-      Ssl.connect ssl;
-    with
-      Ssl.(Connection_error(Error_want_write|Error_want_read|
-                         Error_want_accept|Error_want_connect|Error_zero_return)) ->
-      fn ()
-  in fn ();
-
+  Util.loop (fun () -> Ssl.connect ssl);
   let verify_result =
     try
-      Ssl.verify ssl;
-      ""
+      Util.loop (fun () -> Ssl.verify ssl; "");
     with
     | e -> Printexc.to_string e
   in
-  let rec fn () =
-    try
-      Ssl.shutdown_connection ssl;
-    with
-      Ssl.(Connection_error(Error_want_write|Error_want_read|
-                         Error_want_accept|Error_want_connect|Error_zero_return)) ->
-      fn ()
-  in
-  fn ();
+  Util.loop (fun () -> Ssl.shutdown_connection ssl);
   check
     bool
     "no verify errors"
     true
     (Str.search_forward
-       (Str.regexp_string "error:00000000:lib(0)")
+       (Str.regexp_string "error:00:000000:lib(0)::reason(0)")
        verify_result
        0
-     > 0);
-  Unix.kill pid Sys.sigint;
-  Unix.waitpid [] pid |> ignore
-
+     > 0)
 
 let test_read_write () =
   let addr = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 2344) in
-  let pid = Util.server_thread addr (Some (fun _ -> "received")) in
+  let _ = Util.server_thread addr (Some (fun _ -> "received")) in
 
   let context = Ssl.create_context TLSv1_3 Client_context in
   let ssl = Ssl.open_connection_with_context context addr in
-  Unix.set_nonblock (Ssl.file_descr_of_socket ssl);
   let send_msg = "send" in
   let write_buf = Bytes.create (String.length send_msg) in
-  let rec fn () =
-    try Ssl.write ssl write_buf 0 4 |> ignore;
-    with Ssl.(Write_error(Error_want_write|Error_want_read|
-                          Error_want_accept|Error_want_connect|Error_zero_return)) ->
-      fn ()
-  in fn ();
+  Util.loop (fun () -> Ssl.write ssl write_buf 0 4 |> ignore);
   let read_buf = Bytes.create 8 in
-  let rec fn () =
-    try Ssl.read ssl read_buf 0 8 |> ignore;
-    with Ssl.(Read_error(Error_want_write|Error_want_read|
-                          Error_want_accept|Error_want_connect|Error_zero_return)) ->
-      fn ()
-  in fn ();
-  Ssl.shutdown_connection ssl;
-  check string "received message" "received" (Bytes.to_string read_buf);
-  Unix.kill pid Sys.sigint;
-  Unix.waitpid [] pid |> ignore
+  Util.loop (fun () -> Ssl.read ssl read_buf 0 8 |> ignore);
+  Util.loop (fun () -> Ssl.shutdown_connection ssl);
+  check string "received message" "received" (Bytes.to_string read_buf)
 
 let () =
   run
--- tests/util_rlock.ml
+++ tests/util_rlock.ml
@@ -9,21 +9,44 @@ open Ssl.Runtime_lock
 
 type server_args =
   { address : Unix.sockaddr
+  ; condition : Condition.t
   ; mutex : Mutex.t
   ; parser : (string -> string) option
   }
+
+(** General loop/retry function for tests. Do not use, in real code! One must
+    handle the various exception and count the read/write bytes. *)
+let loop action =
+  let rec fn () =
+    try
+      action ()
+    with
+      Ssl.(Connection_error(Error_want_write|Error_want_read|
+                            Error_want_accept|Error_want_connect)) |
+      Ssl.(Read_error(Error_want_write|Error_want_read|
+                            Error_want_accept|Error_want_connect)) |
+      Ssl.(Write_error(Error_want_write|Error_want_read|
+                            Error_want_accept|Error_want_connect)) |
+      Ssl.(Accept_error(Error_want_write|Error_want_read|
+                        Error_want_accept|Error_want_connect)) ->
+      fn ()
+    | e ->
+       raise e
+  in
+  fn ()
 
 let server_rw_loop ssl parser_func =
   let rw_loop = ref true in
   while !rw_loop do
     try
       let read_buf = Bytes.create 256 in
-      let read_bytes = read ssl read_buf 0 256 in
+      let read_bytes = loop (fun () -> read ssl read_buf 0 256) in
       if read_bytes > 0
       then (
         let input = Bytes.to_string read_buf in
         let response = parser_func input in
-        Ssl.write_substring ssl response 0 (String.length response) |> ignore;
+        loop (fun () ->
+            Ssl.write_substring ssl response 0 (String.length response) |> ignore);
         Ssl.close_notify ssl |> ignore;
         rw_loop := false)
     with
@@ -53,32 +76,31 @@ let server_init args =
 let server_listen args =
   match server_init args with
   | None ->
-    Mutex.unlock args.mutex;
-    Thread.exit () [@warning "-3"]
+     Mutex.unlock args.mutex;
+     Condition.signal args.condition;
+     Thread.exit () [@warning "-3"]
   | Some (socket, context) ->
-    Mutex.unlock args.mutex;
-    let listen = Unix.accept socket in
-    let ssl = embed_socket (fst listen) context in
-    let rec fn () =
-      try
-        accept ssl;
-      with
-      | Ssl.(Connection_error(Error_want_write|Error_want_read|
-                              Error_want_accept|Error_want_connect|Error_zero_return)) ->
-         fn ()
-    (* Exit right away unless we need to rw *)
-    (match args.parser with
-    | Some parser_func -> server_rw_loop ssl parser_func
-    | None ->
-      ();
-      shutdown ssl;
-      Thread.exit () [@warning "-3"])
+     Mutex.unlock args.mutex;
+     Condition.signal args.condition;
+     let listen = Unix.accept socket in
+     let ssl = loop (fun () -> embed_socket (fst listen) context) in
+     Unix.set_nonblock (fst listen);
+     loop (fun () -> accept ssl);
+     (* Exit right away unless we need to rw *)
+     (match args.parser with
+      | Some parser_func -> server_rw_loop ssl parser_func
+      | None ->
+         ();
+         shutdown ssl;
+         Thread.exit () [@warning "-3"])
 
 let server_thread addr parser =
   let mutex = Mutex.create () in
-  let args = { address = addr; mutex; parser } in
-  let thread = Thread.create server_listen args in
   Mutex.lock mutex;
+  let condition = Condition.create () in
+  let args = { address = addr; condition; mutex; parser } in
+  let thread = Thread.create server_listen args in
+  Condition.wait condition mutex;
   thread
 
 let check_ssl_no_error err =
