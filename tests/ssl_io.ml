74a855fc96b61c80d41c0d2a098ec3571b504954
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-03-09 23:22:48 -0700

    ssl: preserve unread bytes on short reads

--- tests/ssl_io.ml
+++ tests/ssl_io.ml
@@ -67,6 +67,33 @@ let test_read_write () =
   Ssl.shutdown_connection ssl;
   check string "received message" "received" (Bytes.to_string read_buf)
 
+let test_read_short_does_not_clobber_tail () =
+  let iterations = 64 in
+  let io_size = 4096 in
+  let base_port = 20000 + (Unix.getpid () mod 10000) in
+  for i = 0 to iterations - 1 do
+    let port = base_port + i in
+    let addr = Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", port) in
+    Util.server_thread addr (Some (fun _ -> "z")) |> ignore;
+
+    let context = Ssl.create_context TLSv1_3 Client_context in
+    let ssl = Ssl.open_connection_with_context context addr in
+    let write_buf = Bytes.make io_size 'x' in
+    let read_buf = Bytes.make io_size '?' in
+    let bytes_read =
+      Ssl.write ssl write_buf 0 io_size |> ignore;
+      Ssl.read ssl read_buf 0 io_size
+    in
+    Ssl.shutdown_connection ssl;
+    check int "short read length" 1 bytes_read;
+    check char "first byte copied" 'z' (Bytes.get read_buf 0);
+    check
+      string
+      "tail preserved"
+      (String.make (io_size - 1) '?')
+      (Bytes.sub_string read_buf 1 (io_size - 1))
+  done
+
 let () =
   run
     "Ssl io functions"
@@ -74,5 +101,9 @@ let () =
       , [ test_case "Verify" `Quick test_verify
         ; test_case "Set host" `Quick test_set_host
         ; test_case "Read write" `Quick test_read_write
+        ; test_case
+            "Short read preserves tail"
+            `Quick
+            test_read_short_does_not_clobber_tail
         ] )
     ]
